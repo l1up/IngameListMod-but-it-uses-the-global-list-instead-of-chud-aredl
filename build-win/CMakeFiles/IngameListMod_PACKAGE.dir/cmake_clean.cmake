452168b8884c1e20ac50e20a8963ba26d27aecd0
l1up <kaufmanndimitri.jr@gmail.com>
2026-07-02 00:01:31 -0300

    asd

--- build-win/CMakeFiles/IngameListMod_PACKAGE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/IngameListMod_PACKAGE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/IngameListMod_PACKAGE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build-win/_deps/nontype_functional-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mel/IngameListMod/build-win/_deps/nontype_functional-src
-# Build directory: /home/mel/IngameListMod/build-win/_deps/nontype_functional-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
